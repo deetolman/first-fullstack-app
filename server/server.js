5b5e669ae8103223a97503a0d0a3afd13386f765
Dyanna Tolman <dyannast@gmail.com>
2018-08-29 17:34:46 -0700

    fixed server.js and get and post working in postman

--- server/server.js
+++ server/server.js
@@ -8,35 +8,67 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-app.use(express.json());
+const pg = require('pg');
+const Client = pg.Client;
+const databaseUrl = 'postgres://localhost:5432/pets';
+const client = new Client(databaseUrl);
+client.connect();
 
-const fs = require('fs');
-const dataPath = 'data/dogs.json';
-
-function readData() {
-
-  const raw = fs.readFileSync(dataPath);
-
-  const data = JSON.parse(raw);
-
-  return data;
-}
-
-app.get('/api/dogs', (req, res) => {
-  const data = readData();
-  res.send(data);
+app.get('/api/adoptable-pets', (req, res) => {
+  client.query(`
+    SELECT
+      id,
+      name,
+      type, 
+      weight,
+      age
+    FROM adoptable_pets;
+  `)
+    .then(result => {
+      res.send(result.rows);
+    })
+    .catch(err => console.log(err));
 });
 
-app.post('/api/dogs', (req, res) => {
-  const data = readData();
-  data.push(req.body);
-  fs.writeFileSync(dataPath, JSON.stringify(data));
+app.get('/api/adoptable-pets/:id', (req, res) => {
+  client.query(`
+    SELECT 
+      id,
+      name, 
+      type,
+      weight,
+      age,
+    FROM pets
+    WHERE id = $1;
+  `,
+  [req.params.id]
+  )
+    .then(result => {
+      res.send(result.rows[0]);
+    })
+    .catch(err => console.log(err));
 
-  res.send(req.body);
 });
-    
-const PORT = 3000;
 
-app.listen(PORT, () => console.log('app running...'));
+app.post('/api/adoptable-pets', (req, res) => {
+  console.log('posting');
+  const body = req.body;
+
+  client.query(`
+    INSERT INTO adoptable_pets (name, type, weight, age)
+    VALUES ($1, $2, $3, $4)
+    RETURNING *;
+  `,
+  [body.name, body.type, body.weight, body.age]
+  )
+    .then(result => {
+      // we always get rows back, in this case we just want first one.
+      res.send(result.rows[0]);
+    })
+    .catch(err => console.log(err));
+});
+
+
+app.listen(3000, () => console.log('app running...'));
 
 
